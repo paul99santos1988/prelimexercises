0a47863502241dffa1833a5a0e57cfc40791061d
Paul Joseph Dar Santos <pauljosephdarsantos@pauls-mbp.home>
2015-06-30 18:14:01 +0800

    second commit

---
+++ PrelimExercises/app/src/main/java/com/garcestechnology/prelimexercises/exer1.java
@@ -0,0 +1,41 @@
+package com.garcestechnology.prelimexercises;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class exer1 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_exer1);
+        //setContentView(R.layout.exer4);
+        setContentView(R.layout.exer5);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_exer1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
